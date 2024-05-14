9020fb9666a9ef88c6f49b5e6615a88ddcad9fc1
mikybars <mperezibars@gmail.com>
2024-05-14 13:36:08 +0200

    Document model

--- src/model/board.js
+++ src/model/board.js
@@ -14,15 +14,26 @@ const EMPTY = null;
 export const emptyBoard = () => new Board();
 
 /**
- * Board.
+ * Represents a game board, typically used in games like Tic Tac Toe.
+ * This class handles the state of the board and provides methods to manipulate and query the board state.
  */
 export class Board {
+  /**
+   * Creates a new instance of the board.
+   * @param state - An optional array representing the initial state of the board.
+   *                 If not provided, initializes the board with 9 empty squares.
+   */
   constructor(state) {
     this.squares = state ?? Array(9).fill(EMPTY);
     this.gameOver = this.#calculateGameOver();
     Object.freeze(this);
   }
 
+  /**
+   * Calculates whether the game has ended either by a win or a draw.
+   * @returns Returns an object indicating the game outcome or undefined if the game isn't over.
+   * @private
+   */
   #calculateGameOver() {
     const winnerCombo = this.#findFirstWinnerCombo();
     const boardIsFull = this.squares.every((square) => square !== EMPTY);
@@ -43,6 +54,11 @@ export class Board {
     }
   }
 
+  /**
+   * Finds the first winning combination on the board.
+   * @returns The index set of the winning combination, or undefined if no winner yet.
+   * @private
+   */
   #findFirstWinnerCombo() {
     return WINNING_COMBINATIONS.find((wc) =>
       wc
@@ -51,20 +67,39 @@ export class Board {
     );
   }
 
+  /**
+   * Checks if all squares on the board are empty.
+   * @returns True if all squares are empty, false otherwise.
+   */
   isEmpty() {
     return this.squares.every((square) => square === EMPTY);
   }
 
+  /**
+   * Determines if a specific square is occupied.
+   * @param   square - The index of the square to check.
+   * @returns True if the square is taken, false otherwise.
+   */
   isTaken(square) {
     return this.squares[square] !== EMPTY;
   }
 
+  /**
+   * Returns indices of empty squares excluding specified indices.
+   * @param   squares - Indices to exclude from the check.
+   * @returns A list of indices that are empty and not excluded.
+   */
   emptySquaresExcluding(...squares) {
     return this.squares
       .flatMap((square, index) => (square === EMPTY ? [index] : []))
       .filter((emptySquare) => !squares.includes(emptySquare));
   }
 
+  /**
+   * Calculates all potential winning moves for a given player symbol.
+   * @param   symbol - The player symbol to check for potential winning moves.
+   * @returns An array of indices where playing the symbol could lead to a win.
+   */
   allWinnerMovesBy(symbol) {
     const winnerInOne = (wc) =>
       wc.filter((i) => this.squares[i] === EMPTY).length === 1 &&
@@ -75,16 +110,30 @@ export class Board {
     );
   }
 
+  /**
+   * Counts the total number of moves made by a given symbol on the board.
+   * @param   symbol - The symbol to count moves for.
+   * @returns Total moves made by the symbol.
+   */
   totalMovesBy(symbol) {
     return this.squares.filter((square) => square === symbol).length;
   }
 
+  /**
+   * Returns a new Board instance representing the state of the board after playing the given moves.
+   * @param   moves - Moves to apply on the board, each move should be an object of the structure {symbol, square}.
+   * @returns A new board instance with the moves applied.
+   */
   afterPlaying(...moves) {
     const newState = [...this.squares];
     moves.forEach(({ symbol, square }) => (newState[square] = symbol));
     return new Board(newState);
   }
 
+  /**
+   * Allows iteration over the board's squares.
+   * @returns An iterator for the squares of the board.
+   */
   [Symbol.iterator]() {
     return this.squares[Symbol.iterator]();
   }
