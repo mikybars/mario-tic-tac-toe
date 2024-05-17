aafebfb7f1f726991b37feac9b42224c1e3b8e20
mikybars <mperezibars@gmail.com>
2024-05-17 07:26:08 +0200

    Bypass punycode deprecation warning temporarily

--- src/components/Board1p.test.jsx
+++ src/components/Board1p.test.jsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
-import { act } from 'react-dom/test-utils'
 import characters from '../characters'
 import { Player as PlayerModel } from '../model/player'
 import { TURN } from '../model/turn'
 import { Board } from './Board'
+import { findCharacter } from '../testUtils'
 
 describe('<Board 1-players />', () => {
   let user
@@ -31,22 +31,20 @@ describe('<Board 1-players />', () => {
     user = userEvent.setup()
   })
 
-  test('player 1 must wait his turn', async() => {
+  test('player 1 must wait his turn after playing', async() => {
     await user.click(squares[0])
     await user.click(squares[1])
 
-    expect(screen.getAllByRole('img')).toHaveLength(1)
-    screen.getByTitle('Mario')
+    expect(await screen.findAllByRole('img')).toHaveLength(1)
+    await findCharacter('Mario')
   })
 
   test('player 2 plays after player 1', async() => {
-    await act(async() => {
-      user.click(squares[0])
+    await user.click(squares[0])
 
-      vi.waitFor(() => {
-        expect(screen.getAllByRole('img')).toHaveLength(2)
-        screen.getByTitle('Bowser')
-      })
+    vi.waitFor(async() => {
+      expect(await screen.findAllByRole('img')).toHaveLength(2)
+      await findCharacter('Bowser')
     })
   })
 })
